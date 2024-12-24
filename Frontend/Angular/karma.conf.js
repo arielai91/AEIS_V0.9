dc3429ef2056a723bf9dc0283f220b91b8c0aa8c
XolHed3RP <105742572+arielai91@users.noreply.github.com>
2024-12-24 10:50:04 -0500

    modify(version 0.1.4): cambios en el CI/CD del frontend

--- Frontend/Angular/karma.conf.js
+++ Frontend/Angular/karma.conf.js
@@ -3,7 +3,7 @@ module.exports = function (config) {
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
-      require("karma-chrome-launcher"), // Plugin para lanzar Chrome
+      require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-coverage"),
       require("@angular-devkit/build-angular/plugins/karma"),
