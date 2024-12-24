e80a8752b405d8066fcf1bd5cf188317fc7a68b7
XolHed3RP <105742572+arielai91@users.noreply.github.com>
2024-12-24 10:59:16 -0500

    modify(version 0.1.4): cambios en el CI/CD

--- Frontend/Angular/karma.conf.js
+++ Frontend/Angular/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
     reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG, // Para obtener más detalles de los errores
     autoWatch: true,
     browsers: ["ChromeHeadless"], // Asegúrate de que solo ChromeHeadless esté listado aquí
     customLaunchers: {
@@ -33,7 +33,7 @@ module.exports = function (config) {
         ],
       },
     },
-    singleRun: false,
+    singleRun: true, // Cambia a `true` para evitar múltiples intentos
     restartOnFileChange: true,
   });
 };
