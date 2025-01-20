e5af22d17ec29e4c54b373ffae585755689f0111
Ariel Justin Amaguaña Toapanta <ariel.amaguana@epn.edu.ec>
2025-01-20 14:30:37 -0500

    update: clean up import statements and ensure consistent formatting in routes and services

--- Backend/Backend-Flask/app/routes/__init__.py
+++ Backend/Backend-Flask/app/routes/__init__.py
@@ -1,6 +1,6 @@
-# Importa las rutas de email definidas en app/routes/email_routes.py
 from .email_routes import email_routes
 from .perfil_routes import perfil_routes
+
 
 def app_routes(app):
     """Registra las rutas en la aplicación."""
