d7d1dc27853fd4a8384b6b0470a1183ce9bed455
Ariel Justin Amaguaña Toapanta <ariel.amaguana@epn.edu.ec>
2025-01-21 11:26:47 -0500

    update: add NgRx entity support and environment configurations for production and development

--- Frontend/Angular/src/app/app.module.ts
+++ Frontend/Angular/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environment/environment';
 
 @NgModule({
   declarations: [
@@ -11,6 +14,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/Angular/src/environment/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api.miapp.com/api' // URL de tu backend en producción
+};
