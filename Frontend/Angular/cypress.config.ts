20980713b957dde71f172050e597be58dc3736d0
Ariel Justin Amaguaña Toapanta <ariel.amaguana@epn.edu.ec>
2025-01-20 21:43:37 -0500

    update: remove unused Angular files and configurations

--- Frontend/Angular/cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      console.log("Setup Cypress Events", { on, config });
-    },
-  },
-});
